42bb045b8b80603a6a9d92dc414de804cabe33d2
ShiruRino <tigugg654@gmail.com>
2025-11-18 07:47:47 +0700

    First commit

---
+++ app/Http/Controllers/DetailOrdeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetailOrde;
+use Illuminate\Http\Request;
+
+class DetailOrdeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DetailOrde $detailOrde)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DetailOrde $detailOrde)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DetailOrde $detailOrde)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DetailOrde $detailOrde)
+    {
+        //
+    }
+}
